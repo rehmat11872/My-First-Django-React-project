12546b9e9a0951ec0b5c9c738fc1297b0dbdef84
RehmatQadeer <82605080+rehmat11872@users.noreply.github.com>
2022-06-07 09:46:30 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import  { Container } from 'react-bootstrap'
+import * as React from 'react';
+import Headers from "./components/Headers";
+import Footer from "./components/Footer";
+
+
+function App() {
+  return (
+    <div>
+     <Headers />
+
+     <main className="py-3">
+      <Container>
+        <h1>Welcome</h1>
+      </Container>
+     </main>
+
+     <Footer />
+    </div>
+  );
+}
+
+export default App;
