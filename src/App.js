f8824e26a124f979664dabb05996bf0d8fd0e331
RehmatQadeer <82605080+rehmat11872@users.noreply.github.com>
2022-06-15 15:58:56 +0000

    Qty select and add to cart

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from "./components/Footer";
 
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
 
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
       
        <Route path='/' element={<HomeScreen />} exact />
        <Route path='/product/:id' element={<ProductScreen/>}/>
+       <Route path='/cart/:id' element={<CartScreen/>}/>
 
     </Routes>
     </Container>
--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -4,6 +4,10 @@ import {
     PRODUCT_LIST_REQUEST,
     PRODUCT_LIST_SUCCESS,
     PRODUCT_LIST_FAIL,
+
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_SUCCESS,
+    PRODUCT_DETAILS_FAIL,
 } from '../constants/productConstants.js'
 
 export const listProducts = () => async (dispatch) => {
@@ -23,6 +27,33 @@ export const listProducts = () => async (dispatch) => {
 
         dispatch({
             type: PRODUCT_LIST_FAIL,
+            payload: error.response && error.response.data.message
+                ? error.response.data.message
+                : error.message,
+        })
+
+    }
+
+}
+
+
+export const listProductDetails = (id) => async (dispatch) => {
+
+    try{
+
+        dispatch({type: PRODUCT_DETAILS_REQUEST})
+
+        const { data } = await axios.get(`/api/products/${id}`)
+
+        dispatch({
+            type:PRODUCT_DETAILS_SUCCESS,
+            payload: data
+        })
+
+    }catch(error){
+
+        dispatch({
+            type: PRODUCT_DETAILS_FAIL,
             payload: error.response && error.response.data.message
                 ? error.response.data.message
                 : error.message,
