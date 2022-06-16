80aea472ed8231a8aa0d8fc92770a9638a300ec9
RehmatQadeer <82605080+rehmat11872@users.noreply.github.com>
2022-06-16 10:45:24 +0000

    Cart Screen

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -4,7 +4,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
 import Message from '../components/Message'
 import { addToCart } from '../actions/cartActions'
-import { useParams, useLocation,  } from "react-router-dom";
+import { useParams, useLocation, useNavigate } from "react-router-dom";
+
 
 function CartScreen() {
   const param = useParams()
@@ -24,12 +25,108 @@ function CartScreen() {
       dispatch(addToCart(productId, qty))
     }
   }, [dispatch, productId,qty])
+
+
+  const removeFromCarthandler = (id) => {
+    console.log('remove', id)
+  }
+   
+  const navigate = useNavigate()
+  const checkoutHandler = () => {
+
+    navigate(`/login?redirect=shopping`)
+
+  }
   
   
   return (
-    <div>
-      Hello product
-    </div>
+    <Row>
+       <Col md={8}>
+          <h1>Shopping Cart</h1>
+          {cartItems.length === 0 ? (
+            <Message variant='info'>
+               Your Cart is empty <Link to='/'>Go Back</Link>
+            </Message>
+          ): (
+              <ListGroup variant=''>
+                 {cartItems.map(item => (
+                   <ListGroup.Item key={item.product}>
+
+                     <Row>
+                        <Col md={2}>
+                            <Image src={item.image} alt={item.name} fluid rounded />
+                        </Col>
+                        <Col md={3}>
+                            <Link to={`/product/${item.product}`}>{item.name}</Link>
+                        </Col>
+                        <Col md={2}>
+                           ${item.price}
+                        </Col>
+
+                        <Col md={3}>
+                           <Form.Control
+                              as="select"
+                              value={item.qty}
+                              onChange = {(e) => dispatch(addToCart(item.product, Number(e.target.value)))}
+                            >
+
+                            {
+                              [...Array(item.countInStock).keys()].map((x) => (
+                                <option key={x + 1} value={x + 1}>
+                                  {x + 1}
+                                </option>
+                              ))
+                            }
+                            
+                            </Form.Control>
+                          </Col>
+                          
+                          <Col md={1}>
+                             <Button 
+                             type='button'
+                             variant='light'
+                             onClick={() => removeFromCarthandler(item.product)}
+                             >
+                             <i className='fas fa-trash'></i>
+                             </Button>
+                          </Col>
+                     
+                     </Row>
+                   
+                   </ListGroup.Item>
+                 ))}
+              </ListGroup>
+
+          )}
+       </Col> 
+
+
+       <Col md={4}>
+            <Card>
+               <ListGroup variant='flush'>
+                  <ListGroup.Item>
+                       <h2>Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0 )}) items</h2>
+                       ${cartItems.reduce((acc, item) => acc + item.qty * item.price, 0 ).toFixed(2)}
+                  </ListGroup.Item>
+               </ListGroup>
+
+               <ListGroup.Item>
+                   <Button
+                   type='button'
+                   className='btn-block'
+                   disabled={cartItems.length === 0 }
+                   onClick={checkoutHandler}
+                   >
+                   Proceed to Checkout
+                   </Button>
+               </ListGroup.Item>
+              
+ 
+
+
+            </Card>
+       </Col> 
+    </Row>
   )
 }
 
